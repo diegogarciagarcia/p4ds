4d2ed3e9b37ce1c014020fd28d567d152dc14760
diegogarcia <diegogarciagarcia@gmail.com>
2018-02-21 13:59:50 +0100

    remove zip and initial test

--- word_count/my_word_cloud.py
+++ word_count/my_word_cloud.py
@@ -4,15 +4,15 @@ Created on Wed Jan 24 20:33:12 2018
 
 @author: dgarciagarci
 """
-
-#import  
+#this is a change
+#import
 import collections
 import io
 
 f = io.open("98-0.txt", mode="r", encoding="utf-8")
 #file=open("98-0.txt","r")
 #file=open("mytxt.txt")
-
+print("empiezo")
 wordcount_dict ={}
 wordcount_list =[]
 wordcount_set = set()
@@ -26,8 +26,8 @@ for word in f.read().lower().split():
     else:
         wordcount_dict[word] += 1
 
-    
-    
+
+
 print("numero en el dictionary=" + str(len(wordcount_dict)))
 print("numero en la lista=" + str(len(wordcount_list)))
 print("numero en set="+str(len(wordcount_set)))
