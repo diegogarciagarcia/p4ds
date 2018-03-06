24a0c97eab7a48714c1f2ea563f7865185a6d769
diegogarcia <diegogarciagarcia@gmail.com>
2018-03-06 12:17:34 +0100

    Multiple tests

---
+++ Tests/tests.py
@@ -0,0 +1,7 @@
+for i in range(0,10):
+    if i % 2 == 0:
+        print(i)
+    elif i % 3 ==0:
+        print(str(i)+" es divisible por 3")
+    else:
+        print(i+10)
---
+++ Tests/unzip_files.py
@@ -0,0 +1,16 @@
+import os, zipfile, sys
+
+dir_name = "C:/Users/dgarciagarci/Downloads/Garmin"
+extension = ".zip"
+
+os.chdir(dir_name) # change directory from working dir to dir with files
+
+for item in os.listdir(dir_name): # loop through items in dir
+    if item.endswith(extension): # check for ".zip" extension
+        file_name = os.path.abspath(item) # get full path of files
+        zip_ref = zipfile.ZipFile(file_name) # create zipfile object
+        zip_ref.extractall(dir_name) # extract file to dir
+        zip_ref.close() # close file
+        os.remove(file_name) # delete zipped file
+        if "-p" in sys.argv:
+            print("selected for printing")
